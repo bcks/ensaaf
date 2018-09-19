6b2a2711a92977221bbf9621c92b63eafbbd754a
bcks <john@backspace.com>
2018-09-19 08:57:22 -0400

    Boost villages in search results
    Signed-off-by: bcks <john@backspace.com>

--- data/search_indexes.py
+++ data/search_indexes.py
@@ -2,7 +2,6 @@ import datetime
 from haystack import indexes
 from .models import *
 
-
 class DataIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.EdgeNgramField(document=True, use_template=True)
     record_id = indexes.CharField(model_attr='record_id', null=True)
@@ -14,3 +13,23 @@ class DataIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self, using=None):
         return self.get_model().objects.all()
+
+class VillageIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.EdgeNgramField(document=True, use_template=True)
+    village_name = indexes.CharField(model_attr='village_name', null=True)
+    id = indexes.CharField(model_attr='id', null=True)
+    tehsil = indexes.CharField(model_attr='tehsil', null=True)
+    district = indexes.CharField(model_attr='district', null=True)
+    district_id = indexes.CharField(model_attr='id', null=True)
+    tehsil_id = indexes.CharField(model_attr='tehsil_id', null=True)
+
+    def get_model(self):
+        return Villages
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+    def prepare(self, obj):
+        data = super(VillageIndex, self).prepare(obj)
+        data['_boost'] = 3
+        return data
