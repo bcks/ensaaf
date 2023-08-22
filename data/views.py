b764a63b959159219f0e5197a4932286b0739518
bcks <john@backspace.com>
2023-08-22 18:54:16 -0400

    Make a robots.txt file and disallow CCBot

--- data/views.py
+++ data/views.py
@@ -537,6 +537,12 @@ def perpetrators(request):
 
 
 @cache_page(60 * 60)
+def robots(request):
+    return render(request, "robots.txt")
+
+
+
+@cache_page(60 * 60)
 def official(request, slug=None):
     name =  officials.get(slug)
 
